00130823c8f96c17c7fdca5607ad54e49c9cbff2
Aaruran <ladchumananaaruran@gmail.com>
2023-05-24 12:02:18 +0530

    1st commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Kanaa</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
