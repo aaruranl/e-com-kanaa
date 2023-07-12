e7856c8a31b86a0c09951a2c77b8006c48df7f39
Aaruran <ladchumananaaruran@gmail.com>
2023-07-12 19:05:49 +0530

    add bootstrap

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'name 1',
+            slug: 'yt1',
+            category: 'shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 7,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high quality',
+        }, 
+        {
+            name: 'name 1',
+            slug: 'yt2',
+            category: 'shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 7,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high quality',
+        }, 
+        {
+            name: 'name 1',
+            slug: 'yt3',
+            category: 'shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 7,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high quality',
+        }, 
+        {
+            name: 'name 1',
+            slug: 'yt4',
+            category: 'shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 7,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high quality',
+        },   
+    ]
+}
+
+export default data;
